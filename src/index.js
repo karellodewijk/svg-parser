cb75d7899cd40b3d155fca25825e2e885b116196
Karel Verdonck <karel.lodewijk@gmail.com>
2021-10-12 15:31:53 +0200

    Unescape html characters in string

--- src/index.js
+++ src/index.js
@@ -4,6 +4,39 @@ const validNameCharacters = /[a-zA-Z0-9:_-]/;
 const whitespace = /[\s\t\r\n]/;
 const quotemark = /['"]/;
 
+var htmlEntities = {
+  nbsp: ' ',
+  cent: '¢',
+  pound: '£',
+  yen: '¥',
+  euro: '€',
+  copy: '©',
+  reg: '®',
+  lt: '<',
+  gt: '>',
+  quot: '"',
+  amp: '&',
+  apos: '\''
+};
+
+function unescapeHTML(str) {
+  return str.replace(/\&([^;]+);/g, function (entity, entityCode) {
+      var match;
+
+      if (entityCode in htmlEntities) {
+          return htmlEntities[entityCode];
+          /*eslint no-cond-assign: 0*/
+      } else if (match = entityCode.match(/^#x([\da-fA-F]+)$/)) {
+          return String.fromCharCode(parseInt(match[1], 16));
+          /*eslint no-cond-assign: 0*/
+      } else if (match = entityCode.match(/^#(\d+)$/)) {
+          return String.fromCharCode(~~match[1]);
+      } else {
+          return entity;
+      }
+  });
+};
+
 function repeat(str, i) {
 	let result = '';
 	while (i--) result += str;
@@ -45,7 +78,7 @@ export function parse(source) {
 		while (i < source.length && source[i] !== '<') text += source[i++];
 
 		if (/\S/.test(text)) {
-			currentElement.children.push({ type: 'text', value: text });
+			currentElement.children.push({ type: 'text', value: unescapeHTML(text) });
 		}
 
 		if (source[i] === '<') {
